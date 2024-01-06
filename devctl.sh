6a66fc28f4ffb5e3f08e84d2c0c213e0d3c6fcc2
Paul Mielle <pm.mielle@gmail.com>
2024-01-06 08:59:58 +0100

    cicd v2

--- devctl.sh
+++ devctl.sh
@@ -14,19 +14,6 @@ reinit() { stop "$1"; sleep 5; init "$1"; }
 # -----------------------------------------------
 docker network create sonddr >/dev/null 2>&1 || true
 
-# special subcommand "up" + authority
-# n.b. early exit
-if [[ "$1" = "up" ]]; then
-	export SONDDR_AUTHORITY="${2:-$SONDDR_AUTHORITY}"
-	reinit auth
-	reinit database
-	reinit api 
-	reinit webapp
-	reinit reverse-proxy
-	exit 0
-fi
-
-# otherwise service-level subcommand
 subcommand="$1"
 service="$2"
 
