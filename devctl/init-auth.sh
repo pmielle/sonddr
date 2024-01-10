abd8781470977784fab35f071164d94c05423810
Paul Mielle <pm.mielle@gmail.com>
2024-01-10 13:58:09 +0100

    realm json

--- devctl/init-auth.sh
+++ devctl/init-auth.sh
@@ -9,23 +9,8 @@ if [[ -z ${SONDDR_AUTHORITY:-} ]]; then echo "SONDDR_AUTHORITY env var is missin
 # --http-relative-path must match the reverse proxy path
 docker run --quiet -d --rm --name auth \
 	--network sonddr \
+	--mount="type=bind,source=./auth/sonddr-realm.json,target=/opt/keycloak/data/import/sonddr-realm.json" \
 	--env KEYCLOAK_ADMIN=admin --env KEYCLOAK_ADMIN_PASSWORD=admin \
+	--env SONDDR_AUTHORITY --env SONDDR_BACKEND_SECRET --env SONDDR_GOOGLE_SECRET \
 	quay.io/keycloak/keycloak:22.0.1 \
-	start-dev  --http-relative-path /auth
-
-echo "sleeping 120s to let keycloak init..." # 60s is sometimes too short
-sleep 120
-
-kcadm="/opt/keycloak/bin/kcadm.sh"
-kcadm_auth="--server http://localhost:8080/auth --realm master --user admin --password admin"
-
-docker exec auth bash -c "$kcadm create realms $kcadm_auth -s realm=sonddr -s enabled=true"
-
-docker exec auth bash -c "$kcadm create users $kcadm_auth -r sonddr -s username=test -s enabled=true"
-docker exec auth bash -c "$kcadm set-password $kcadm_auth -r sonddr --username test --new-password test"
-
-docker exec auth bash -c "$kcadm create users $kcadm_auth -r sonddr -s username=test2 -s enabled=true"
-docker exec auth bash -c "$kcadm set-password $kcadm_auth -r sonddr --username test2 --new-password test2"
-
-docker exec auth bash -c "$kcadm create clients $kcadm_auth -r sonddr -s clientId=sonddr-backend -s standardFlowEnabled=false -s enabled=true"
-docker exec auth bash -c "$kcadm create clients $kcadm_auth -r sonddr -s clientId=sonddr-frontend -s 'redirectUris=[\"http://$SONDDR_AUTHORITY/*\"]' -s 'webOrigins=[\"http://$SONDDR_AUTHORITY\"]' -s directAccessGrantsEnabled=true -s publicClient=true -s enabled=true"
+	start-dev  --import-realm --http-relative-path /auth
