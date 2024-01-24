f57c50343eb0fb310d85cd3eaa30614173870caf
Paul Mielle <pm.mielle@gmail.com>
2024-01-24 19:11:19 +0100

    do not bind mongo anymore

--- devctl/init-reverse-proxy.sh
+++ devctl/init-reverse-proxy.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 if [[ -z ${SONDDR_AUTHORITY:-} ]]; then echo "SONDDR_AUTHORITY env var is missing" >&2; exit 1; fi
 
 docker run --quiet -d --rm --name reverse-proxy \
-	--network sonddr -p 80:80 -p 27017:27017 \
+	--network sonddr -p 80:80 \
 	--mount type=bind,source=./reverse-proxy/nginx.conf,target=/etc/nginx/nginx.conf \
 	--env SONDDR_AUTHORITY=$SONDDR_AUTHORITY \
 	nginx
