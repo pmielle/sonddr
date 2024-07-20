e26073377c74fb02645b9d5d7d139e441a49ec92
Paul Mielle <pm.mielle@gmail.com>
2024-07-20 21:07:08 +0200

    persist uploads

--- deployment/utils/up_all.sh
+++ deployment/utils/up_all.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-docker compose -f ./deployment/compose.yaml -f ./deployment/compose-dev.yaml up -d
-echo "sleeping 10s to let auth init..."
-sleep 10
+docker compose -f ./deployment/compose.yaml -f ./deployment/compose-dev.yaml up -d --build
+echo "sleeping 20s to let auth init..."
+sleep 20
 ./deployment/utils/create-test-users.sh
