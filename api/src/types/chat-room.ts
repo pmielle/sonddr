69c1062d7acea6551765bf6e8f28b23ed2235146
Paul Mielle <pm.mielle@gmail.com>
2024-12-02 07:32:11 +0100

    chatroom debug and refactor

--- api/src/types/chat-room.ts
+++ api/src/types/chat-room.ts
@@ -1,7 +1,7 @@
 import { WebSocket } from "ws";
-import { Subscription, filter as rxFilter, switchMap } from "rxjs";
-import { Change, DbMessage, Message, placeholder_id } from "sonddr-shared";
-import { getDocuments } from "../database.js";
+import { Subscription, filter as rxFilter } from "rxjs";
+import { Change, DbMessage, DbReaction, delete_react_str, delete_str, Message, placeholder_id, react_str, sep_str } from "sonddr-shared";
+import { getDocument, getDocuments, patchDocument, postDocument } from "../database.js";
 import { messagesChanges$ } from "../triggers/triggers.js";
 import { reviveMessage, reviveMessages } from "../revivers/messages.js";
 import { reviveChange } from "../revivers/changes.js";
@@ -13,11 +13,11 @@ export class ChatRoomManager {
 
 	constructor() {}
 
-	getOrCreateRoom(discussionId: string, userId: string, socket: WebSocket): ChatRoom {
+	joinOrCreateRoom(discussionId: string, userId: string, socket: WebSocket): ChatRoom {
 		let room: ChatRoom;
 		if (this.rooms.has(discussionId)) {
 			room = this.rooms.get(discussionId);
-			room.join(userId, socket);
+			room.connect(userId, socket);
 		} else {
 			room = new ChatRoom(discussionId, userId, socket);  // no need to join the room after, it is done in init()
 			this.rooms.set(discussionId, room);
@@ -25,14 +25,14 @@ export class ChatRoomManager {
 		return room;
 	}
 
-	leaveRoom(room: ChatRoom, userId: string) {
-		room.leave(userId);
-		if (room.isEmpty) {
-			room.disable();  // cancel any subscription before gc
-			this.rooms.delete(room.discussionId);
-		}
-	}
-
+    leaveRoom(discussionId: string, userId: string) {
+        let room = this.rooms.get(discussionId);
+        room.disconnect(userId);
+        if (room.isEmpty()) {
+            room.disable();
+            this.rooms.delete(discussionId);
+        }
+    }
 }
 
 export class ChatRoom {
@@ -46,13 +46,14 @@ export class ChatRoom {
 		this._init(firstUserId, firstUserSocket);
 	}
 
-	async join(userId: string, socket: WebSocket) {
+	async connect(userId: string, socket: WebSocket) {
 		const oldMessages = await this._getOldMessages(userId);
 		this._send(oldMessages, socket);
 		this.clients.set(userId, socket);
+        socket.on("message", async (data) => this._recieve(data.toString(), userId));
 	}
 
-	leave(userId: string) {
+	disconnect(userId: string) {
 		this.clients.delete(userId);
 	}
 
@@ -66,22 +67,9 @@ export class ChatRoom {
 
 	// private
 	// ------------------------------------------
-	_send(payload: Message[]|Change<Message>, socket: WebSocket) {
-		socket.send(JSON.stringify(payload));
-	}
-
 	async _init(firstUserId: string, firstUserSocket: WebSocket) {
-		await this.join(firstUserId, firstUserSocket);
+		await this.connect(firstUserId, firstUserSocket);
 		this._listenToDatabase();
-	}
-
-	async _getOldMessages(userId: string): Promise<Message[]> {
-		const docs = await getDocuments<DbMessage>(
-			"messages",
-			{field: "date", desc: true},
-			{field: "discussionId", operator: "eq", value: this.discussionId},
-		).then(dbDocs => reviveMessages(dbDocs, userId));
-		return docs;
 	}
 
 	_listenToDatabase() {
@@ -102,6 +90,81 @@ export class ChatRoom {
 		});
 	}
 
+    async _recieve(message: string, userId: string) {
+        if (message.startsWith(delete_str)) {
+            const messageId = message.split(sep_str)[1];
+            this._delete(messageId);
+        } else if (message.startsWith(react_str)) {
+            const [messageId, emoji] = message.split(sep_str).slice(1,3);
+            this._react(messageId, emoji, userId);
+        } else if (message.startsWith(delete_react_str)) {
+            const messageId = message.split(sep_str)[1];
+            this._deleteReaction(messageId, userId);
+        } else {
+            this._post(message, userId);
+        }
+    }
+
+    async _post(message: string, userId: string) {
+        const newMessagePayload = {
+            discussionId: this.discussionId,
+            authorId: userId,
+            date: new Date(),
+            content: message,
+            deleted: false,
+        };
+        const newMessageId = await postDocument('messages', newMessagePayload);
+        patchDocument(
+            `discussions/${this.discussionId}`,
+            [
+                { field: "lastMessageId", operator: "set", value: newMessageId },
+                { field: "readByIds", operator: "set", value: [userId] },
+                { field: "date", operator: "set", value: newMessagePayload.date },
+            ]
+        );
+    }
+
+    async _delete(messageId: string) { // TODO: only allow author
+        await patchDocument(
+            `messages/${messageId}`,
+            [
+                { field: "deleted", operator: "set", value: true },
+                { field: "content", operator: "set", value: "Deleted" },
+            ]
+        );
+    }
+
+    async _react(messageId: string, emoji: string, userId: string) {
+        let dbMessage = await getDocument<DbMessage>(`messages/${messageId}`);
+        let reactions = this._addToReactions(dbMessage.reactions, emoji, userId);
+        await patchDocument(
+            `messages/${messageId}`,
+            [ { field: "reactions", operator: "set", value: reactions } ]
+        );
+    }
+
+    async _deleteReaction(messageId: string, userId: string) { // TODO: only allow author
+        let dbMessage = await getDocument<DbMessage>(`messages/${messageId}`);
+        let reactions = this._removeReaction(dbMessage.reactions, userId);
+        await patchDocument(
+            `messages/${messageId}`,
+            [ { field: "reactions", operator: "set", value: reactions } ]
+        );
+    }
+
+	_send(payload: Message[]|Change<Message>, socket: WebSocket) {
+		socket.send(JSON.stringify(payload));
+	}
+
+	async _getOldMessages(userId: string): Promise<Message[]> {
+		const docs = await getDocuments<DbMessage>(
+			"messages",
+			{field: "date", desc: true},
+			{field: "discussionId", operator: "eq", value: this.discussionId},
+		).then(dbDocs => reviveMessages(dbDocs, userId));
+		return docs;
+	}
+
 	_getDiscussionIdOfDbChange(change: Change<DbMessage>): string {
 		const discussionId = change.docBefore?.discussionId || change.docAfter?.discussionId;
 		if (! discussionId) { throw new Error("Failed to find discussionId of change"); }
@@ -114,4 +177,40 @@ export class ChatRoom {
 		return authorId;
 	}
 
+    _addToReactions(reactions: DbReaction[]|undefined, emoji: string, userId: string): DbReaction[] {
+        // first time someone ever react to this message
+        if (!reactions) {
+            return [{emoji: emoji, fromUserIds: [userId]}];
+        }
+        // if user has already reacted, remove their previous reaction
+        reactions = this._removeReaction(reactions, userId) || [];
+        // add new reaction to preexisting list
+        let reaction = reactions.find(r => r.emoji === emoji);
+        if (reaction) {
+            // someone has already reacted with this emoji
+            if (reaction.fromUserIds.includes(userId)) {
+                throw new Error(`${userId} has already reacted with ${emoji}`);
+            }
+            reaction.fromUserIds.push(userId);
+        } else {
+            // first time someone reacts with this emoji
+            reactions.push({emoji: emoji, fromUserIds: [userId]});
+        }
+        return reactions;
+    }
+
+    _removeReaction(reactions: DbReaction[], userId: string): DbReaction[]|undefined {
+        let previousReaction = reactions.find(r => r.fromUserIds.includes(userId));
+        if (previousReaction) {
+            // remove the user from the list
+            previousReaction.fromUserIds = previousReaction.fromUserIds.filter(id => id !== userId);
+            // remove this reaction entirely if it was the only user
+            if (previousReaction.fromUserIds.length === 0) {
+                reactions = reactions.filter(r => r !== previousReaction);
+            }
+            // return undefined if empty
+            if (reactions.length === 0) { return undefined; }
+        }
+        return reactions;
+    }
 }
--- api/src/websockets.ts
+++ api/src/websockets.ts
@@ -1,15 +1,13 @@
 import { IncomingMessage } from "http";
 import { Server } from "node:http";
 import { WebSocketServer } from "ws";
-import { DbMessage, DbReaction, delete_react_str, delete_str, react_str, sep_str } from "sonddr-shared";
 import { authenticateIncomingMessage } from "./auth.js";
 import { wsBasePath } from "./routes/routes.js";
-import { ChatRoom, ChatRoomManager } from "./types/chat-room.js";
-import { getDocument, patchDocument, postDocument } from "./database.js";
+import { ChatRoomManager } from "./types/chat-room.js";
 
 export function addWebsockets(server: Server) {
 
-	const messagesWss = new WebSocketServer({ noServer: true });
+	const wss = new WebSocketServer({ noServer: true });
 
 	server.on("upgrade", async (incomingMessage, duplex, buffer) => {
 		duplex.on("error", (err) => console.error(err));
@@ -19,8 +17,8 @@ export function addWebsockets(server: Server) {
 			await authenticateIncomingMessage(incomingMessage);
 
 			if (incomingMessage.url!.startsWith(`${wsBasePath}/messages`)) {
-				messagesWss.handleUpgrade(incomingMessage, duplex, buffer, (ws) => {
-					messagesWss.emit('connection', ws, incomingMessage);
+				wss.handleUpgrade(incomingMessage, duplex, buffer, (ws) => {
+					wss.emit('connection', ws, incomingMessage);
 				});
 			} else {
 				throw new Error(`Unexpected websocket url: ${incomingMessage.url}`);
@@ -31,74 +29,14 @@ export function addWebsockets(server: Server) {
 		}
 	});
 
-	const roomManager = new ChatRoomManager();
+	const manager = new ChatRoomManager();
 
-	messagesWss.on('connection', (ws, incomingMessage) => {
+	wss.on('connection', (ws, incomingMessage) => {
 
 		const userId = incomingMessage["userId"];
 		const discussionId = _getFromIncomingMessageQuery<string>("discussionId", incomingMessage);
 
-		const room: ChatRoom = roomManager.getOrCreateRoom(discussionId, userId, ws);
-		// n.b. no need to send previous messages, ChatRoom does it
-
-		ws.on("message", async (data) => {
-
-			const message = data.toString();
-
-			if (message.startsWith(delete_str)) {
-
-                const messageId = message.split(sep_str)[1];
-				await patchDocument(
-					`messages/${messageId}`,
-					[
-						{ field: "deleted", operator: "set", value: true },
-						{ field: "content", operator: "set", value: "Deleted" },
-					]
-				);
-
-            } else if (message.startsWith(react_str)) {
-
-                const [messageId, emoji] = message.split(sep_str).slice(1,3);
-                let dbMessage = await getDocument<DbMessage>(`messages/${messageId}`);
-                let reactions = _addToReactions(dbMessage.reactions, emoji, userId);
-				await patchDocument(
-					`messages/${messageId}`,
-					[ { field: "reactions", operator: "set", value: reactions } ]
-				);
-
-			} else if (message.startsWith(delete_react_str)) {
-
-                const messageId = message.split(sep_str)[1];
-                let dbMessage = await getDocument<DbMessage>(`messages/${messageId}`);
-                let reactions = _removeReaction(dbMessage.reactions, userId);
-				await patchDocument(
-					`messages/${messageId}`,
-					[ { field: "reactions", operator: "set", value: reactions } ]
-				);
-
-            } else {
-
-				const newMessagePayload = {
-					discussionId: discussionId,
-					authorId: userId,
-					date: new Date(),
-					content: message,
-					deleted: false,
-				};
-				const newMessageId = await postDocument('messages', newMessagePayload);
-				patchDocument(
-					`discussions/${discussionId}`,
-					[
-						{ field: "lastMessageId", operator: "set", value: newMessageId },
-						{ field: "readByIds", operator: "set", value: [userId] },
-						{ field: "date", operator: "set", value: newMessagePayload.date },
-					]
-				);
-
-			}
-			// n.b. no need to dispatch anything, ChatRoom reacts to database changes
-
-		});
+		manager.joinOrCreateRoom(discussionId, userId, ws);
 
 		// heartbeat to keep the connection alive
 		// otherwise nginx timeouts after 60s
@@ -106,49 +44,13 @@ export function addWebsockets(server: Server) {
 
 		ws.on("close", () => {
 			clearInterval(pingId);  // stop ping loop
-			room.leave(userId);
+            manager.leaveRoom(discussionId, userId);
 		});
 
 	});
 
 }
 
-function _addToReactions(reactions: DbReaction[]|undefined, emoji: string, userId: string): DbReaction[] {
-    // first time someone ever react to this message
-    if (!reactions) {
-        return [{emoji: emoji, fromUserIds: [userId]}];
-    }
-    // if user has already reacted, remove their previous reaction
-    reactions = _removeReaction(reactions, userId) || [];
-    // add new reaction to preexisting list
-    let reaction = reactions.find(r => r.emoji === emoji);
-    if (reaction) {
-        // someone has already reacted with this emoji
-        if (reaction.fromUserIds.includes(userId)) {
-            throw new Error(`${userId} has already reacted with ${emoji}`);
-        }
-        reaction.fromUserIds.push(userId);
-    } else {
-        // first time someone reacts with this emoji
-        reactions.push({emoji: emoji, fromUserIds: [userId]});
-    }
-    return reactions;
-}
-
-function _removeReaction(reactions: DbReaction[], userId: string): DbReaction[]|undefined {
-    let previousReaction = reactions.find(r => r.fromUserIds.includes(userId));
-    if (previousReaction) {
-        // remove the user from the list
-        previousReaction.fromUserIds = previousReaction.fromUserIds.filter(id => id !== userId);
-        // remove this reaction entirely if it was the only user
-        if (previousReaction.fromUserIds.length === 0) {
-            reactions = reactions.filter(r => r !== previousReaction);
-        }
-        // return undefined if empty
-        if (reactions.length === 0) { return undefined; }
-    }
-    return reactions;
-}
 
 function _getFromIncomingMessageQuery<T>(key: string, incomingMessage: IncomingMessage): T {
 	const url = new URL(incomingMessage.url, `http://${incomingMessage.headers.host}`);
